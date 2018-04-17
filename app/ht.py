ad56ac5278345fdfbc96213cfaea7d7ef786da82
z30832564 <630832564@qq.com>
2018-04-17 22:29:55 +0800

    Add files via upload

--- app/ht.py
+++ app/ht.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
 from app.search.colordescriptor import ColorDescriptor
 from app.search.searcher import Searcher
-
+import jieba
+from app.kg.knowledgeg import Queryer
+import string
 import cv2
 import os
 
@@ -14,19 +16,41 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        txt = request.form.get("content")
+        print(txt)
+        return redirect(url_for('search_text', string=txt))
+    return render_template('index.html')
+
+
+@app.route('/t_img', methods=["GET", "POST"])
+def t_img():
+    if request.method == "POST":
+        return redirect(url_for('index_img'))
+    return render_template('index.html')
+
+
+@app.route('/t_text', methods=["GET", "POST"])
+def t_text():
+    if request.method == "POST":
+        return redirect(url_for('index'))
+    return render_template('index1.html')
+
+
+@app.route('/index_img', methods=["GET", "POST"])
+def index_img():
+    if request.method == "POST":
         image = request.files['file']
         # 保存图像
         filename = secure_filename(image.filename)
         image.save(filename)
+        return redirect(url_for('search_file', filename=filename))
 
-        return redirect(url_for('search', filename=filename))
-
-    return render_template('index.html')
+    return render_template('index1.html')
 
 
 # 搜索页面部分
-@app.route('/searchs/<filename>', methods=['POST', 'GET'])
-def search(filename):
+@app.route('/search_file/<filename>', methods=['POST', 'GET'])
+def search_file(filename):
     query = cv2.imread(filename)
     # 实例化ColorDescriptor类，bin的数量值
     cd = ColorDescriptor((8, 3, 3))
@@ -37,5 +61,15 @@ def search(filename):
     s = Searcher(features)
     s.Search()
     results = s.results
+    print(results)
     # 传递参数给html
-    return render_template('result.html', results=results, filename=filename)
+    return render_template('result.html', results=results)
+
+
+@app.route('/search_text/<string>', methods=['POST', 'GET'])
+def search_text(string):
+    # l = list(jieba.cut_for_search(string))
+    l = string.split('为')
+    q = Queryer(l)
+    result = q.Query()
+    return render_template('result.html', results=result)
--- app/index.py
+++ app/index.py
@@ -12,14 +12,30 @@ index = db.features.tindex
 
 cd = ColorDescriptor((8, 3, 3))
 
+
+import pymongo
+import pandas as pd
+data = pd.read_table('data1.txt', sep='\s+')
+columns = list(data.columns)
+# mongodb数据库初始化
+
 # glob遍历数据集图片，生成数据集存入MongoDB数据库中
-for imagePath in glob.glob("/home/search/static/dataset" + "/*.jpg"):
+def jstz(imagePath):
     ID = imagePath[imagePath.rfind("/") + 1:]
-    imageID = ID[:6]
+    imageID = ID[:10]
     image = cv2.imread(imagePath)
     # #提取特征值
-    features = cd.describe(image)
-    features = [float(f) for f in features]
-    f = {"_id": imageID, imageID: features}
+    _features = cd.describe(image)
+    _features = [float(f) for f in _features]
+    return imageID, _features
+
+
+for i in range(len(data)):
+    f = {}
+    for j in range(len(columns)):
+        f[columns[j]] = str(data.loc[i][j])
+    path = str(data.loc[i][4])
+    imageID_,features_ = jstz(imagePath=path)
+    f['_id'] = imageID_
+    f[imageID_] = features_
     index.insert_one(f)
-    print(imageID)
